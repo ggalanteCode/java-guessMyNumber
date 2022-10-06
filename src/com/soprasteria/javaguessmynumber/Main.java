a8f32d2f8553de6fe4acac09be6477c805d8ae36
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-06 14:37:12 +0200

    prova ad indovinare il numero

--- src/com/soprasteria/javaguessmynumber/Main.java
+++ src/com/soprasteria/javaguessmynumber/Main.java
@@ -1,14 +1,20 @@
 package com.soprasteria.javaguessmynumber;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Random r = new Random();
+		Scanner s = new Scanner(System.in);
 		
 		int numeroRandom = r.nextInt(10);
+		
+		System.out.print("Prova ad indovinare il numero: ");
+		int tentativo = s.nextInt();
+		s.nextLine();
 	}
 
 }
