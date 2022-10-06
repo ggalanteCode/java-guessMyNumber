536d646c2a215b3a4b2c8edd1ddd920f7108aabf
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-06 14:31:59 +0200

    estrazione di un numero random

--- src/com/soprasteria/javaguessmynumber/Main.java
+++ src/com/soprasteria/javaguessmynumber/Main.java
@@ -1,10 +1,14 @@
 package com.soprasteria.javaguessmynumber;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Random r = new Random();
 		
+		int numeroRandom = r.nextInt(10);
 	}
 
 }
